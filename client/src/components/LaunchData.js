ea1955be7aa5fee8ef382eb5a6dfbde1c20a0a7f
victorjaquez <victor@jaquez.me>
2019-01-08 10:15:39 -0700

    Mission status key, classname colorscheme for launch result

--- client/src/components/LaunchData.js
+++ client/src/components/LaunchData.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classname";
 
 export default function LaunchData({
   launch: { flight_number, mission_name, launch_date_local, launch_success }
@@ -7,7 +8,17 @@ export default function LaunchData({
     <div className="card card-body mb-3">
       <div className="row">
         <div className="col-md-9">
-          <h4>Mission: {mission_name} </h4>
+          <h4>
+            Mission:{" "}
+            <span
+              className={classNames({
+                "text-success": launch_success,
+                "text-danger": !launch_success
+              })}
+            >
+              {mission_name}
+            </span>{" "}
+          </h4>
           <p>Date: {launch_date_local}</p>
         </div>
         <div className="col-md-3">
