0eadcc9763d3e49b76cbb62be03a6e67e79f3d93
victorjaquez <victor@jaquez.me>
2019-01-08 10:20:08 -0700

    Used Moment to make data and time readable

--- client/src/components/LaunchData.js
+++ client/src/components/LaunchData.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classNames from "classname";
+import Moment from "react-moment";
 
 export default function LaunchData({
   launch: { flight_number, mission_name, launch_date_local, launch_success }
@@ -19,7 +20,9 @@ export default function LaunchData({
               {mission_name}
             </span>{" "}
           </h4>
-          <p>Date: {launch_date_local}</p>
+          <p>
+            Date: <Moment format="YYYY-MM-DD HH:mm">{launch_date_local}</Moment>
+          </p>
         </div>
         <div className="col-md-3">
           <button className="btn btn-secondary">Launch Details</button>
