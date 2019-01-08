24e55c868122f4b5d6368a64de359e1115d11ea6
victorjaquez <victor@jaquez.me>
2019-01-08 11:06:14 -0700

    path module typo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const cors = require("cors");
-const schema = require("./schema"); 
-const path - require('path'); // core nodejs module to work w/ file path
+const schema = require("./schema");
+const path = require("path"); // core nodejs module to work w/ file path
 
 const app = express();
 
@@ -13,17 +13,17 @@ app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: schema,
+    schema,
     graphiql: true
   })
 );
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
